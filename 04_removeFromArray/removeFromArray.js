e0fe10e508aaf87d037013ed17c827a5c0689172
Nogiback <peter.toan.do@gmail.com>
2023-07-05 15:04:42 -0400

    complete 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(givenArray, ...removeItems) {
+    //initializing new array for return
+    let newArray = [];
+    //looping through givenArray parameter
+    for (const element of givenArray) {
+        //checking if any elements in the givenArray are also in removeItems
+        if(removeItems.includes(element)) {
+            continue;
+        } else {
+            //Push element to newArray if removeItems are not found in givenArray
+            newArray.push(element);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
