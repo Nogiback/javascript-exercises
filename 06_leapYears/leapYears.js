d50ec09c5288cdccbe54fa5497294f9107c86fad
Nogiback <peter.toan.do@gmail.com>
2023-07-05 17:10:04 -0400

    complete exercise 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let isLeapYear;
+    if (year%4==0 && (year%100!==0 || year%400==0)) {
+        return isLeapYear = true;
+    } else {
+        return isLeapYear = false;
+    }
 };
 
 // Do not edit below this line
