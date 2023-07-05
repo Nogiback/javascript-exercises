edc2350b96f8408d96579e611799bf609e0e8ecd
Nogiback <peter.toan.do@gmail.com>
2023-07-05 17:36:43 -0400

    Complete exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  let tempCelsius = 0;
+  let roundedTempCelsius = 0;
+
+  tempCelsius = ((tempFahrenheit-32)*(5/9));
+
+  roundedTempCelsius = Math.round(tempCelsius*10) / 10;
+
+  return roundedTempCelsius;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  let tempFahrenheit = 0;
+  let roundedTempFahrenheit = 0;
+
+  tempFahrenheit = (tempCelsius*(9/5)+32);
+
+  roundedTempFahrenheit = Math.round(tempFahrenheit*10) / 10;
+
+  return roundedTempFahrenheit;
+  
 };
 
 // Do not edit below this line
