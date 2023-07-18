93fd851dafc18b2f9a96d4438d0e99670c5d37ba
Nogiback <peter.toan.do@gmail.com>
2023-07-18 16:50:49 -0400

    Completed exercise 10-fiboacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  let a = 0;
+  let b = 1;
+  let fib;
 
+  num = parseInt(num);
+
+  if (num < 0) {
+    return "OOPS";
+  } else if (num === 1) {
+    return 1;
+  } else {
+    for (let i = 1; i < num; i++) {
+      fib = a + b;
+      a = b;
+      b = fib;
+    };
+    return fib;
+  };
 };
 
 // Do not edit below this line
